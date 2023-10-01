b563bd566577d61aaed055cf61a9b2edae9621bd
danvs6 <wgj6fc@virginia.edu>
2023-10-01 13:19:38 -0400

    Implemented port scanner in ScanPorts

--- src/main/java/scanner/port/ScanPorts.java
+++ src/main/java/scanner/port/ScanPorts.java
@@ -2,18 +2,34 @@ package scanner.port;
 
 import java.net.Socket;
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class ScanPorts {
-    private String targetHost;
-    private int startPort;
-    private int endPort;
 
-    public ScanPorts(String targetHost, int startPort, int endPort) // ScanPorts constructor
+    public void beginPortScan(String targetHost, int startPort, int endPort)
     {
-        this.targetHost = targetHost;
-        this.startPort = startPort;
-        this.endPort = endPort;
+        DateTime date = new DateTime();
+        DateTimeFormatter dateTimeFormat = date.dateTimeFormat;
+        LocalDateTime todaysdate = date.todaysDate;
+
+        System.out.println("Scanning started on " + date.SetDateTime(dateTimeFormat,todaysdate));
+
+        for (int port = startPort; port <= endPort; port++)
+        {
+            try
+            {
+                Socket socket = new Socket(targetHost, port);
+                System.out.println("Port " + port + "is open");
+                socket.close();
+            }
+            catch(IOException e)
+            {
+                System.out.println("Port " + port + "is closed");
+            }
+
+        }
+
+        System.out.println("Port Scanning Completed");
     }
-
-
 }
