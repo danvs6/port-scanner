58245414bac868d7f91bb332b200db1e218707c1
danvs6 <wgj6fc@virginia.edu>
2023-10-01 12:44:52 -0400

    Renamed InputPorts to ScanPorts

--- src/main/java/scanner/port/ScanPorts.java
+++ src/main/java/scanner/port/ScanPorts.java
@@ -1,14 +1,19 @@
 package scanner.port;
 
-public class InputPorts {
+import java.net.Socket;
+import java.io.IOException;
 
+public class ScanPorts {
     private String targetHost;
     private int startPort;
     private int endPort;
 
-    public InputPorts(String targetHost, int startPort, int endPort){
+    public ScanPorts(String targetHost, int startPort, int endPort) // ScanPorts constructor
+    {
         this.targetHost = targetHost;
         this.startPort = startPort;
         this.endPort = endPort;
     }
+
+
 }
